22afe27bca2c828f622bde31108d6aed0342539f
Prasad Poul <prasadpoul.2024.com>
2024-08-18 11:42:01 +0530

    Seventh Assignment

---
+++ Task1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+
+//Using Directory, File, and FileInfo Classes
+
+class Task1
+{
+    static void Main()
+    {
+        string mainFolder = "demo";
+        string subFolder1 = Path.Combine(mainFolder, "demo1");
+        string subFolder2 = Path.Combine(mainFolder, "demo2");
+
+        // Create the main folder and two subfolders
+        Directory.CreateDirectory(mainFolder);
+        Directory.CreateDirectory(subFolder1);
+        Directory.CreateDirectory(subFolder2);
+
+        // Create some files in demo1 folder
+        string filePath1 = Path.Combine(subFolder1, "file1.txt");
+        string filePath2 = Path.Combine(subFolder1, "file2.txt");
+
+        // Create and write to file using File class
+        File.WriteAllText(filePath1, "Hello from file1!");
+
+        // Create and write to file using FileInfo class
+        FileInfo fileInfo = new FileInfo(filePath2);
+        using (StreamWriter writer = fileInfo.CreateText())
+        {
+            writer.WriteLine("Hello from file2!");
+        }
+
+        // Copy file2.txt to demo2 folder
+        string copyFilePath = Path.Combine(subFolder2, "file2_copy.txt");
+        File.Copy(filePath2, copyFilePath);
+
+        // Show all files and folders in the main folder
+        Console.WriteLine("Contents of 'demo' folder:");
+        ShowContents(mainFolder);
+
+        // Show creation time of files and folders
+        Console.WriteLine("\nCreation times:");
+        ShowCreationTimes(mainFolder);
+
+        // Delete files and folders
+        Console.WriteLine("\nCleaning up...");
+        DeleteAllFiles(mainFolder);
+        DeleteAllFolders(mainFolder);
+
+        Console.WriteLine("All files and folders deleted.");
+    }
+
+    static void ShowContents(string path)
+    {
+        foreach (string dir in Directory.GetDirectories(path))
+        {
+            Console.WriteLine("Directory: " + dir);
+        }
+        foreach (string file in Directory.GetFiles(path))
+        {
+            Console.WriteLine("File: " + file);
+        }
+    }
+
+    static void ShowCreationTimes(string path)
+    {
+        foreach (string dir in Directory.GetDirectories(path))
+        {
+            Console.WriteLine(dir + " was created on: " + Directory.GetCreationTime(dir));
+        }
+        foreach (string file in Directory.GetFiles(path))
+        {
+            Console.WriteLine(file + " was created on: " + File.GetCreationTime(file));
+        }
+    }
+
+    static void DeleteAllFiles(string path)
+    {
+        foreach (string file in Directory.GetFiles(path))
+        {
+            File.Delete(file);
+        }
+    }
+
+    static void DeleteAllFolders(string path)
+    {
+        foreach (string dir in Directory.GetDirectories(path))
+        {
+            Directory.Delete(dir, true);
+        }
+        Directory.Delete(path);
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
+using System.IO;
+
+class Task2
+
+{
+    static void Main()
+    {
+        string fileName = string.Empty;
+
+        while (true)
+        {
+            Console.WriteLine("\nMenu:");
+            Console.WriteLine("1. Create a new file");
+            Console.WriteLine("2. Write text to the file");
+            Console.WriteLine("3. Add more text to the file");
+            Console.WriteLine("4. Show text line by line");
+            Console.WriteLine("5. Show all text at once");
+            Console.WriteLine("6. Exit");
+            Console.Write("Choose an option (1-6): ");
+
+            int choice = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                switch (choice)
+                {
+                    case 1:
+                        Console.Write("Enter file name: ");
+                        fileName = Console.ReadLine();
+                        using (FileStream fs = new FileStream(fileName, FileMode.Create))
+                        {
+                            Console.WriteLine($"File '{fileName}' created.");
+                        }
+                        break;
+
+                    case 2:
+                        Console.Write("Enter text to write: ");
+                        string text = Console.ReadLine();
+                        using (StreamWriter sw = new StreamWriter(fileName))
+                        {
+                            sw.WriteLine(text);
+                        }
+                        Console.WriteLine("Text written to the file.");
+                        break;
+
+                    case 3:
+                        Console.Write("Enter text to append: ");
+                        string appendText = Console.ReadLine();
+                        using (StreamWriter sw = new StreamWriter(fileName, true))
+                        {
+                            sw.WriteLine(appendText);
+                        }
+                        Console.WriteLine("Text appended to the file.");
+                        break;
+
+                    case 4:
+                        using (StreamReader sr = new StreamReader(fileName))
+                        {
+                            string line;
+                            while ((line = sr.ReadLine()) != null)
+                            {
+                                Console.WriteLine(line);
+                            }
+                        }
+                        break;
+
+                    case 5:
+                        using (StreamReader sr = new StreamReader(fileName))
+                        {
+                            string content = sr.ReadToEnd();
+                            Console.WriteLine("File content:");
+                            Console.WriteLine(content);
+                        }
+                        break;
+
+                    case 6:
+                        return;
+
+                    default:
+                        Console.WriteLine("Please choose a valid option.");
+                        break;
+                }
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine("Error: File not found.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+    }
+}
+
